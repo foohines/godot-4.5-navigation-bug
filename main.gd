00f533d3553a2bd3b09fe82ac7584bf099e406af
Foster Hines <hinesfoster73@gmail.com>
2025-09-22 05:54:07 -0700

    add static bodies

--- main.gd
+++ main.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
-@onready var agent: NavigationAgent2D = $Player/NavigationAgent2D
+@onready var nav_region: NavigationRegion2D = $NavigationRegion2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-func _unhandled_input(event: InputEvent) -> void:
-	if (event is InputEventMouseButton && event.is_pressed() && (event as InputEventMouseButton).button_index == MOUSE_BUTTON_LEFT):
-		agent.target_position = get_global_mouse_position()
+	nav_region.bake_navigation_polygon()
